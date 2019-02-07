9e888c7f1724ef97d2c0b022b7334b16151c3de1
Karsten Jeschkies <kjeschkies@mesosphere.com>
2019-02-07 16:43:51 +0100

    Support pods.

--- tests/performance/marathon.py
+++ tests/performance/marathon.py
@@ -44,6 +44,22 @@ class AppObject:
                 await resp.text()
 
 
+class PodObject:
+
+    def __init__(self, base, pod):
+        self._base = base
+        self._pod = pod
+
+    async def delete(self):
+        async with aiohttp.ClientSession() as session:
+            pod_id = self._pod['id']
+            logger.info('Deleting %s...', pod_id)
+            path = '{}/v2/pods/{}'.format(self._base, pod_id)
+            async with session.delete(path) as resp:
+                logger.info('Done deleting %s: %d', pod_id, resp.status)
+                await resp.text()
+
+
 class Apps(Collection):
 
     def __init__(self, base):
@@ -67,7 +83,26 @@ class Apps(Collection):
                 return AppObject(self._base, app_spec)
 
 
-# class Pods(Collection):
+class Pods(Collection):
+    def __init__(self, base):
+        self._base = base
+
+    async def all(self):
+        async with aiohttp.ClientSession() as session:
+            logger.info('Fetching all pods...')
+            async with session.get("{}/v2/pods".format(self._base)) as resp:
+                pods = await resp.json()
+                for pod_spec in pods:
+                    yield PodObject(self._base, pod_spec)
+
+    async def create(self, pod_spec):
+        async with aiohttp.ClientSession() as session:
+            pod_id = pod_spec['id']
+            logger.info('Posting pod %s', pod_id)
+            async with session.post("{}/v2/pods".format(self._base), json=pod_spec) as resp:
+                assert resp.status == 201, 'Marathon replied with {}:{}'.format(resp.status, await resp.text())
+                logger.info('Done posting %s: %d', pod_id, resp.status)
+                return PodObject(self._base, pod_spec)
 
 
 class Marathon:
@@ -78,13 +113,18 @@ class Marathon:
     def apps(self):
         return Apps(self._base)
 
-    def pods():
-        pass
+    def pods(self):
+        return Pods(self._base)
 
 
 def app_ids(number):
     for i in range(number):
         yield '/simpleapp-{}'.format(i)
+
+
+def pod_ids(number):
+    for i in range(number):
+        yield '/simplepod-{}'.format(i)
 
 
 def app_spec(app_id):
@@ -96,6 +136,19 @@ def app_spec(app_id):
             "executor": "/opt/shared/marathon_performance_executor-1.5.1",
             "labels": {"MARATHON_EXECUTOR_ID": "custom-executor"}
             }
+
+
+def pod_spec(pod_id):
+    return { "id": pod_id,
+             "scaling": { "kind": "fixed", "instances": 0 },
+	     "containers": [
+                { "name": "sleep1",
+	          "exec": { "command": { "shell": "sleep 100000000" } },
+	          "resources": { "cpus": 0.1, "mem": 10 }
+                }
+	     ],
+	     "networks": [ {"mode": "host"} ]
+	  }
 
 
 async def create_apps(number):
@@ -112,17 +165,36 @@ async def create_apps(number):
         asyncio.ensure_future(create(spec))
 
 
+async def create_pods(number):
+    client = Marathon(MARATHON_BASE)
+    for pod_id in pod_ids(number):
+        logger.info('Create pod with id %s', pod_id)
+        spec = pod_spec(pod_id)
+
+        @retry(before_sleep=before_sleep_log(logger, logging.DEBUG), before=before_log(logger, logging.DEBUG), wait=wait_fixed(2))
+        async def create(spec):
+            await client.pods().create(spec)
+
+        # Fire and forget.
+        asyncio.ensure_future(create(spec))
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
 
     command = sys.argv[1]
     if command == "create":
-        apps = int(sys.argv[2])
-        loop.run_until_complete(create_apps(apps))
+        typ = sys.argv[2]
+        count = int(sys.argv[3])
+        if typ == 'apps':
+            loop.run_until_complete(create_apps(count))
+        else:
+            loop.run_until_complete(create_pods(count))
     else:
         client = Marathon(MARATHON_BASE)
         loop.run_until_complete(client.apps().delete_all())
+        loop.run_until_complete(client.pods().delete_all())
 
     # Let's also finish all running tasks:
     pending = asyncio.Task.all_tasks()
